b5d7163e3198e0ef80df7892629b0c8ad7515d90
Herr Gromov <youriy.gromov@gmail.com>
2022-07-09 12:07:20 +0200

    finish secon part about riturn in function

--- Unit-3/Return.js
+++ Unit-3/Return.js
@@ -46,3 +46,85 @@ function () {
 function clearText(data) {
     return data.trim().toLowerCase(); //задача 1 - очистить пробелы
 }
+
+
+// Return прерывает выполнение функции
+
+
+
+//Прерывание функции
+//Эмулирование indexOf
+function indexOfEmul(arr, num) {
+for (let i = 0; i < arr.lenght; i++) {
+if(arr[i] === num) return i;
+}
+    return -1;
+}
+let result = indexOfEmul([22,33,44,55], 44);
+console.log(result);
+
+
+//Пользователь вводит кол-во лет.
+document.querySelector('.b-4').addEventListener('click', 
+function () {
+    let year = +document.querySelector('.i-4').value;
+    if (isNaN(year)) return;
+    if (year <0 || year > 140) return;
+    document.querySelector('.out-4').innerHTML = 2021 - year;
+});
+
+
+//Ex 5 - Применение суммы
+console.group('example 5');
+const arr5 = [ [3,4,5], [6,7,8] ];
+function t5() {
+    let s = 0;
+    for (let i = 0; i < arr5.length; i++) {
+        s += sum(arr5[i]);
+    }
+    return s;
+}
+
+function sum(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(t5());
+console.groupEnd();
+
+
+// return и ;
+//Ex 5 - Применение суммы
+console.group('example 6');
+function t6() {
+    let a = 88;
+    /** перенос - ошибка 
+     * return
+     * a;
+     */     
+    return a;
+}
+console.groupEnd();
+
+
+//Ex 7 Return
+console.group('example 7');
+const arr7 = [[3, 4, 5], [6, 7, 9], [7, 7, 1], [2, 2]];
+
+const evenArr7 = arr7.filter(function(item) {
+    return(sum(item) % 2 === 0);
+});
+console.log(evenArr7);
+console.groupEnd();
+
+// Exercise 8 return функции
+function t8() {
+    return sum;
+}
+
+let s = t8();
+console.log(s ([10,20,30]));
