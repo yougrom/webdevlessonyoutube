131b87c79d64f33987494b5d0bd54c608091394d
Herr Gromov <youriy.gromov@gmail.com>
2022-07-15 18:25:11 +0200

    2/2 of sesson about function  bind, call, apply

--- unit_6/index.js
+++ unit_6/index.js
@@ -1,5 +1,5 @@
 //контекст функций - это облость видимости переменных для функции и + переменная this 
-//this это ссылка на объект который вызывает код в данный момент
+//this - это ссылка на объект который вызывает код в данный момент
 
 let count = 0;
 function f1() {
@@ -24,7 +24,7 @@ function f1() {
 // document.querySelector('.b-2').addEventListener('click', f2);
 
 
-//что такое call - позволяет вызывать функцию и передовать ей необходимые документы
+//что такое call - позволяет вызывать функцию и передовать ей необходимые оргументы
 //call позволяет подминять контекст функции при запуске
 f1.call(document.querySelector('.b-1'));
 
@@ -52,5 +52,30 @@ function sum1(a, b) {
 }
 
 document.querySelector('.b-2').addEventListener('click', ()=>{
-    sum1(3,4);
+    sum1.call(document.querySelector('.out-3'), 17,4);
+    sum1.apply(document.querySelector('.out-4'), [17,4]);
+
+});
+
+//метод bind
+const f4 = f1.bind(document.querySelector('.out-5'));
+f4();
+document.querySelector('.b-5').addEventListener('click', f4);
+
+
+//Example 6
+const sum2 = sum1.bind(document.querySelector('.out-6'), 77, 88);
+sum2();
+
+// частичные функции или функции с переопределённым документом
+
+function sum3(a, b, c) {
+    console.log(arguments);//arguments это похожая на моссив конструкция которая содержим переданные в sum3 аргументы
+    this.innerHTML = a + b + c;
+}
+
+const sum4 = sum3.bind(document.querySelector('.b-6'), 100, 200);
+
+document.querySelector('.b-6').addEventListener('click', ()=>{
+sum4(3);
 });
